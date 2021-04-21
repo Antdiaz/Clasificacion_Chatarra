e400072e42528058135ce806974d1ec7f313596e
antdiaz <antdia@deacero.com>
2021-04-21 11:06:56 -0500

    commit

--- Recoleccion-master/Views/src/views/Usuarios/Usuarios.js
+++ Recoleccion-master/Views/src/views/Usuarios/Usuarios.js
@@ -1,125 +1,170 @@
-import React from 'react';
-import { Card, CardHeader, CardBody, Row, Col, CardTitle } from 'reactstrap';
-import DataGrid, { Column } from 'devextreme-react/data-grid';
+import React,{useState} from 'react';
+import DataGrid from 'devextreme-react/data-grid';
 import { config } from '../../utils/config';
 import { callApi, callKrakenApi } from '../../utils/utils';
+import { Card, CardText, CardHeader, CardBody, CardTitle, Button, Row, Col} from 'reactstrap';
+import { useParams } from 'react-router';
+import PlacaInfo from './PlacaInfo';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import EditIcon from '@material-ui/icons/Edit';
+import Popup from './Popup';
 
-export default class Responsables extends React.Component {
-  constructor(props) {
-    super(props);
+function Usuarios({Valores}) {
+    const {id} = useParams();
+    const Val= localStorage.getItem(Valores)
+    const buttonOptions = {
+      text: 'Siguiente \u2192',
+  };
 
-    this.state = {
-      responseKraken: [],
-    };
+  
+
+function createData(Fabricacion,MaterialE,MaterialR,Porcentaje,CantidadE,CantidadR,KilosE,KilosR,AtrilE,AtrilR,InventarioA,InventarioS) {
+  return {Fabricacion,MaterialE,MaterialR,Porcentaje,CantidadE,CantidadR,KilosE,KilosR,AtrilE,AtrilR,InventarioA,InventarioS};
+  }
+const rows = [
+  createData('123456','Mixto para procesar','Mixto para procesar', 24,15000,14500,36000,25000,0,0,'X','Y'),
+  createData('223456','Mixto para procesar','Mixto para procesar', 24,15000,14500,36000,25000,0,0,'X','Y'),
+  ];
+
+const [isOpen, setIsOpen] = useState(false);
+ 
+const togglePopup = () => {
+  setIsOpen(!isOpen);
   }
 
-  componentDidMount() {
-    const urlWebService = `${config.UrlApiProject}usuario/getUsuariosRol`;
-    const urlKrakenService = `${config.KrakenService}/${8}/${7}`;
-
-    const paramsService = {
-      columnas: 'ClaUsuario, NomUsuario, Email',
-      condicion: "NomUsuario LIKE '%dario %' ORDER BY NomUsuario ASC",
-      tipoEstructura: 5,
-    };
-
-    callApi(urlKrakenService, 'POST', paramsService, (res) => {
-      this.setState({
-        responseKraken: res,
-      });
-    });
-
-    callApi(urlWebService, 'POST', null, (res) => {
-      this.setState({
-        response: res.data,
-      });
-    });
-  }
-
-  render() {
-    const GridKrakenContent = (
-      <DataGrid
-        id="gridUsuarios"
-        dataSource={this.state.responseKraken}
-        keyExpr="ClaUsuario"
-        height={600}
-        showBorders={false}
-        columnHidingEnabled
-        columnAutoWidth={false}
-        showColumnHeaders
-        showColumnLines={false}
-        showRowLines
-        noDataText="Sin Registros"
-        wordWrapEnabled
-        rowAlternationEnabled={false}
-      >
-        <Column dataField="ClaUsuario" caption="Clave del Usuario" width={100} alignment="center" />
-        <Column dataField="NomUsuario" caption="Nombre" width={200} />
-        <Column dataField="Email" caption="Email" />
-      </DataGrid>
-    );
-
-    const GridServicioContent = (
-      <DataGrid
-        id="gridServicio"
-        dataSource={this.state.response}
-        keyExpr="claUsuario"
-        height={600}
-        showBorders={false}
-        columnHidingEnabled
-        columnAutoWidth={false}
-        showColumnHeaders
-        showColumnLines={false}
-        showRowLines
-        noDataText="Sin Registros"
-        wordWrapEnabled
-        rowAlternationEnabled={false}
-      >
-        <Column dataField="claUsuario" caption="Clave del Usuario" width={100} alignment="center" />
-        <Column dataField="nomUsuario" caption="Nombre" width={200} />
-        <Column dataField="rol" caption="Rol" />
-      </DataGrid>
-    );
-
-    const GridKrakenDisplay = () => <div>{GridKrakenContent}</div>;
-
-    const GridServicioDisplay = () => <div>{GridServicioContent}</div>;
 
     return (
       <>
-        <div className="content">
+      
+        <div className="content" style={{marginTop: "20px"}}>
+          <Row style={{alignItems:"center", justifyContent:"center"}}>
+            <Card className="placa-imagenes">
+              <CardHeader>
+                <CardTitle>Foto Placa</CardTitle>
+              </CardHeader>
+              <CardBody className="p-2">
+                <img style={{ width: '100%' }} src="https://www.supraciclaje.com/wp-content/uploads/2017/06/precio-compra-metales-no-ferrosos-reciclados-mexico.jpg" alt="" />
+                <CardText className="mb-2 text-center">
+                </CardText>
+                <div className="text-center mt-2">
+                </div>
+              </CardBody>
+            </Card>
+            <Card className="placa-imagenes">
+              <CardHeader>
+                <CardTitle>Material Superior</CardTitle>
+              </CardHeader>
+              <CardBody className="p-2">
+                <img style={{ width: '100%' }} src="https://www.supraciclaje.com/wp-content/uploads/2017/06/precio-compra-metales-no-ferrosos-reciclados-mexico.jpg" alt="" />
+                <CardText className="mb-2 text-center">
+                </CardText>
+                <div className="text-center mt-2">
+                </div>
+              </CardBody>
+            </Card>
+            <Card className="placa-imagenes">
+              <CardHeader>
+                <CardTitle>Proveedor / Pre-registro</CardTitle>
+              </CardHeader>
+              <CardBody className="p-2">
+                <img style={{ width: '100%' }} src="https://www.supraciclaje.com/wp-content/uploads/2017/06/precio-compra-metales-no-ferrosos-reciclados-mexico.jpg" alt="" />
+                <CardText className="mb-2 text-center">
+                </CardText>
+                <div className="text-center mt-2">
+                </div>
+              </CardBody>
+            </Card>
+            <Card className="placa-imagenes">
+              <CardHeader>
+                <CardTitle>Manual Oficial</CardTitle>
+              </CardHeader>
+              <CardBody className="p-2">
+                <img style={{ width: '100%' }} src="https://www.supraciclaje.com/wp-content/uploads/2017/06/precio-compra-metales-no-ferrosos-reciclados-mexico.jpg" alt="" />
+                <CardText className="mb-2 text-center">
+                </CardText>
+                <div className="text-center mt-2">
+                </div>
+              </CardBody>
+            </Card>
+          </Row>
           <Row>
-            <Col md={6}>
+            <Col>
               <Card>
                 <CardHeader>
-                  <CardTitle>Peticiones a Servicio en Kraken</CardTitle>
+                  <CardTitle style={{margin:"10px"}}>
+                    <i className="fa fa-plus" aria-hidden="true"></i>
+                    <span style={{marginLeft:"50px"}}>Clasificar Material</span>
+                    <span style={{marginLeft:"50px"}}>Placa: Plac-323</span>
+                    <span style={{marginLeft:"50px"}}>Boleta:{id}</span>
+                  </CardTitle>
                 </CardHeader>
                 <CardBody>
                   <Row>
-                    <Col md={{ size: 10, offset: 1 }}>
-                      <GridKrakenDisplay />
-                    </Col>
-                  </Row>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col md={6}>
-              <Card>
-                <CardHeader>
-                  <CardTitle>Peticiones a Servicio en Net Core</CardTitle>
-                </CardHeader>
-                <CardBody>
-                  <Row>
-                    <Col md={{ size: 10, offset: 1 }}>
-                      <GridServicioDisplay />
+                    <Col md={{ size: 12, offset: 0 }}>
+                      <TableContainer component={Paper}>
+                        <Table className="table" aria-label="a dense table">
+                          <TableHead>
+                            <TableRow>
+                              <TableCell className="table-header">Fabricación</TableCell>
+                              <TableCell className="table-header">Material</TableCell>
+                              <TableCell className="table-header">Porcentaje</TableCell>
+                              <TableCell className="table-header">Cantidad</TableCell>
+                              <TableCell className="table-header">Kilos</TableCell>
+                              <TableCell className="table-header">Atril/Tarima</TableCell>
+                              <TableCell className="table-header">Inventario</TableCell>
+                              <TableCell>          </TableCell>
+                            </TableRow>
+                          </TableHead>
+                          <TableBody>
+                            {rows.map((row) => (
+                              <TableRow key={row.Fabricacion}>
+                                <TableCell className="table-content" component="th" scope="row">{row.Fabricacion}<br /> Renglon: 5</TableCell>
+                                <TableCell className="table-content">Enviado:&nbsp;{row.MaterialE} <br /> Recibido:&nbsp;{row.MaterialR}</TableCell>
+                                <TableCell className="table-content" style={{paddingLeft:"70px", fontWeight: "600"}}>{row.Porcentaje}&nbsp;%</TableCell>
+                                <TableCell className="table-content">Enviado:&nbsp;{row.CantidadE}&nbsp;lb<br /> Recibido:&nbsp;{row.CantidadR}&nbsp;lb</TableCell>
+                                <TableCell className="table-content">Enviado:&nbsp;{row.KilosE}&nbsp;kgs<br />Recibido:&nbsp;{row.KilosR}&nbsp;kgs</TableCell>
+                                <TableCell className="table-content">Enviado:&nbsp;{row.AtrilE}&nbsp;kgs<br />Recibido:&nbsp;{row.AtrilR}&nbsp;kgs</TableCell>
+                                <TableCell className="table-content">Almacen:&nbsp;{row.InventarioA}<br />Sub-Almacen:&nbsp;{row.InventarioS}</TableCell>
+                                <TableCell className="table-content"><div onClick={togglePopup}><EditIcon style={{color:"#ff6a00",cursor:"pointer"}} /></div></TableCell>
+                              </TableRow>
+                            ))}
+                          </TableBody>
+                        </Table>
+                      </TableContainer>
                     </Col>
                   </Row>
                 </CardBody>
               </Card>
             </Col>
           </Row>
+          <Row>
+            <Col>
+              <Card>
+                <CardHeader>
+                  <CardTitle style={{margin:"10px"}}>
+                    <span>Datos de la Unidad</span>
+                  </CardTitle>
+                </CardHeader>
+                <CardBody>
+                  <Row>
+                    <Col md={{ size: 12, offset: 0 }}>
+                      <PlacaInfo listas={Valores && Valores.filter((lista) =>lista.ClaVehiculoPorClasificar === id)} />
+                    </Col>
+                  </Row>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+          {isOpen && <Popup handleClose={togglePopup} /> }
         </div>
       </>
     );
-  }
 }
+
+export default Usuarios
