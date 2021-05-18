0fb920ff6751ee2fbd50521ec16843fc433b816d
antdiaz <antdia@deacero.com>
2021-05-18 17:34:31 -0500

    commit

--- Recoleccion-master/Views/src/views/Usuarios/Warning.js
+++ Recoleccion-master/Views/src/views/Usuarios/Warning.js
@@ -7,6 +7,65 @@ function Warning({poppesaje,setpoppesaje,pesajeparcial,setpesajeparcial}) {
     }
 
     const handlecancel = () => {
+      const urlKrakenService = `${config.KrakenService}/${24}/${37}`;
+
+      
+      const data12 = {
+        parameters:
+          '{"ClaUbicacion":' +
+          props.editBoxValue +
+          ',"ClaServicioJson":12,"Parametros":"@pnClaUbicacion=' +
+          props.editBoxValue +
+          ',@pnIdBoleta=' +
+          props.placadato[0].IdBoleta +
+          ',@pnClaArticuloCompra=' +
+          props.ro.ClaArticuloCompra +
+          ',@pnCantidadMaterial=' +
+          props.ro.CantidadMaterial +
+          ',@pnKilosMaterial=' +
+          props.ro.KilosMaterial +
+          ',@pnKilosReales=' +
+          kilos +
+          ',@pnKilosContaminados=' +
+          kiloscont +
+          ',@pnKilosDocumentados=' +
+          props.ro.KilosDocumentados +
+          ',@pnPorcentajeMaterial=' +
+          porcentajer +
+          ',@pnEsPesajeParcial=' +
+          (props.ro.EsPesajeParcial ? props.ro.EsPesajeParcial : pesajeparcial) +
+          ',@pnClaAlmacen=' +
+          (props.ro.ClaAlmacen ? props.ro.ClaAlmacen : 0) +
+          ',@pnClaSubAlmacenCompra=' +
+          [props.placadato[0].ClaSubAlmacenCompra] +
+          ',@pnClaMotivoContaminacion=' +
+          razoncont +
+          ',@pnEsNoCargoDescargoMaterial=' +
+          props.placadato[0].EsNoCargoDescargoMaterial +
+          ',@pnClaProveedor=' +
+          props.placadato[0].ClaProveedor +
+          ',@pnIdListaPrecio=' +
+          props.placadato[0].IdListaPrecio +
+          ',@pnClaTipoOrdenCompra=,@pnClaOrdenCompra=,@pnClaUbicacionProveedor=' +
+          props.placadato[0].ClaUbicacionProveedor +
+          ',@psClaReferenciaCompra=' +
+          '' +
+          props.ro.ClaReferenciaCompra +
+          Referencia +
+          ',@pnIdRenglon=' +
+          props.ro.IdRenglon +
+          ',@psNombrePcMod=' +
+          ipadress +
+          ',@pnClaUsuarioMod=' +
+          NumbUsuario +
+          ',@pnAccionSp=3"}',
+        tipoEstructura: 0,
+      };
+      /* eslint-enable */
+  
+      callApi(urlKrakenService, 'POST', data12, (res) => {
+        console.log(res);
+      });
       setpesajeparcial(0)
       setpoppesaje(!poppesaje)
       }
